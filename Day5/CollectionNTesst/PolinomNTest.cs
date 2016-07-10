7ae6017a0ee916baf64b7847fc9a07da4682731a
AntonPavl <pavlenok.anton@gmail.com>
2016-07-10 14:23:47 +0300

    Temp

---
+++ Day5/CollectionNTesst/PolinomNTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using PolinomCollection;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CollectionNTesst
+{
+    [TestFixture]
+    public class PolinomNTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var test = new Polinom(new double[3] {0,10,1});
+            Debug.WriteLine($"Elements = {test.Calculate(2)}");
+            var a = new double[2] { 1, 1 };
+            var b = new double[2] { 1, 1 };
+            Assert.AreEqual(Enumerable.SequenceEqual(a, b), true);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            var test = new Polinom(new double[6] { 0, 10, 1,0,-3,-5 });
+            var test2 = new Polinom(new double[6] { 0, -10, -1, 0, 3, 6 });
+            test += test2;
+            Debug.WriteLine($"Elements = {test.ToString()}");
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Day5/Polinom/Polinom.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolinomCollection
+{
+    public class Polinom
+    {
+        public readonly double[] elements;
+        public Polinom(double[] polinom)
+        {
+            elements = polinom;
+        }
+        public double Calculate(double num)
+        {
+            if (elements == null) throw new ArgumentNullException();
+            double result = 0;
+            for (int i = 0; i < elements.Length; i++)
+            {
+                result += elements[i] * Math.Pow(num, i);
+            }
+            return result;
+        }
+
+        public override bool Equals(object obj) 
+        {
+            if (obj == null || !(obj is Polinom))
+                return false;
+            else
+                return Enumerable.SequenceEqual(elements,((Polinom)obj).elements);
+        }
+
+        public override string ToString()
+        {
+            if (elements == null) return "";
+            string result = String.Empty;
+            for (int i = 0; i < elements.Length; i++)
+            {
+               if (elements[i]!=0)
+               {
+                    result += $"{elements[i]}*X^{i} ";
+               }    
+            }
+            return result;
+        }
+
+        public static Polinom operator +(Polinom lp, Polinom rp)
+        {
+            return new Polinom(Sum(lp.elements,rp.elements));
+        }
+
+        private static double[] Sum(double[] p1,double[] p2)
+        {
+            double[] result;
+            if (p1.Length == p2.Length)
+            {
+                result = new double[p1.Length];
+                for (int i = 0; i < p1.Length; i++)
+                {
+                    result[i] = p1[i] + p2[i];
+                }
+            }
+            else if (p1.Length>p2.Length)
+            {
+                result = new double[p1.Length];
+                for (int i = 0; i < p2.Length; i++)
+                {
+                    result[i] = p1[i] + p2[i];
+                }
+                for (int i = p2.Length; i < p1.Length; i++)
+                {
+                    result[i] = p1[i];
+                }
+            }
+            else
+            {
+                result = new double[p2.Length];
+                for (int i = 0; i < p1.Length; i++)
+                {
+                    result[i] = p1[i] + p2[i];
+                }
+                for (int i = p1.Length; i < p2.Length; i++)
+                {
+                    result[i] = p2[i];
+                }
+            }
+            return result;
+        }
+    }
+}
